159b5f7e5371b6e808f3eef350d622ebb4a4c0c6
mperezpizarro <marioperezpizarro@gmail.com>
2024-06-04 10:49:13 +0200

    Changed <a> to <NuxtLink>

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -27,18 +27,23 @@
                 <ul
                     class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white">
                     <li>
-                        <a href="/"
+                        <NuxtLink to="/"
                         class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
-                            aria-current="page">Home</a>
+                        aria-current="page">
+                            Home
+                        </NuxtLink>
                     </li>
                     <li>
-                        <a href="/about"
-                            class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">About</a>
+                        <NuxtLink to="/about"
+                        class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">
+                            About
+                        </NuxtLink>
                     </li>
                     <li>
-                        <a href="#"
-                            class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">GitHub
-                            Repository</a>
+                        <NuxtLink to="#"
+                            class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">
+                            GitHub Repository
+                        </NuxtLink>
                     </li>
                 </ul>
             </div>
