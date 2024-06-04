0d26433a2115617757e514610719e0415d60cb84
mperezpizarro <marioperezpizarro@gmail.com>
2024-06-04 10:50:13 +0200

    Completed about page texts

--- pages/about.vue
+++ pages/about.vue
@@ -22,14 +22,13 @@
     </header>
     <article class="w-full max-w-[80ch] mx-auto">
         <p class="text-lg mb-6">
-            This website helps you create metadata tags for your Matroska video files.
-            It is orientated to movies and TV episodes so it includes fields like the actors, the directors, the title of the movie/episode, the plot...
+            With the program opened, click into the "Add files" button and select your desired files. You can also drag and drop the files you want into the program.
         </p>
         <p class="text-lg mb-6">
-            Once you have filled all the required fields and the optional ones you want, you will be able to download an XML file to add to your .mkv video.
+            Once you have finished your Matroska file, go to the "Output" tab and click the button in the "Add metatags" option to add your previously generated XML file.
         </p>
         <p class="text-lg mb-6">
-            You can use a software like <a href="https://mkvtoolnix.download/" target="_blank">MKVToolnix</a> to add your generated XML to the Matroska file you want.
+            Now you have and MKV file with your desired metadata.
         </p>
     </article>
 </template>
