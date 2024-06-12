de2a64fcec370f1952066a7edb4074d6f96e7e5f
mperezpizarro <marioperezpizarro@gmail.com>
2024-06-12 10:13:15 +0200

    Fixed "required" property

--- components/Input.vue
+++ components/Input.vue
@@ -17,6 +17,6 @@ const props = defineProps({
         <label :for="name" class="block mb-2 text-sm font-medium text-gray-900">{{ label }}</label>
         <input :type="type" :id="id"
             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-            :placeholder="placeholder" :required="required ? 'true' : 'false'" />
+            :placeholder="placeholder" :required="required ? true : false" />
     </div>
 </template>
