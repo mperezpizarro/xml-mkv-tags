77c432d09fe26b87866b5267e16fad060383f979
mperezpizarro <marioperezpizarro@gmail.com>
2024-06-12 10:38:12 +0200

    Fixed v-model binding problem

--- components/Input.vue
+++ components/Input.vue
@@ -10,12 +10,14 @@ const props = defineProps({
         type: String
     }
 })
+
+const model = defineModel()
 </script>
 
 <template>
     <div>
         <label :for="name" class="block mb-2 text-sm font-medium text-gray-900">{{ label }}</label>
-        <input :type="type" :id="id"
+        <input v-model="model" :type="type" :id="id"
             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
             :placeholder="placeholder" :required="required ? true : false" />
     </div>
--- components/TextArea.vue
+++ components/TextArea.vue
@@ -6,11 +6,13 @@ const props = defineProps({
     placeholder: String,
     required: Boolean
 })
+
+const model = defineModel()
 </script>
 
 <template>
     <label :for="name" class="block mb-2 text-sm font-medium text-gray-900">{{ label }}</label>
-    <textarea :id="id" rows="4"
+    <textarea v-model="model" :id="id" rows="4"
         class="col-span-2 block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
         :placeholder="placeholder" :required="required ? true : false"></textarea>
 </template>
