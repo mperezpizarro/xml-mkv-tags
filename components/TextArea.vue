c6bea0177ef0773107461909121164d591579dc4
mperezpizarro <marioperezpizarro@gmail.com>
2024-06-14 09:51:01 +0200

    Modified component props

--- components/TextArea.vue
+++ components/TextArea.vue
@@ -4,7 +4,10 @@ const props = defineProps({
     name: String,
     id: String,
     placeholder: String,
-    required: Boolean
+    required: Boolean,
+    disabled: Boolean,
+    resize: "y" | "none",
+    rows: String
 })
 
 const model = defineModel()
@@ -12,7 +15,7 @@ const model = defineModel()
 
 <template>
     <label :for="name" class="block mb-2 text-sm font-medium text-gray-900">{{ label }}</label>
-    <textarea v-model="model" :id="id" rows="4"
-        class="col-span-2 block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
-        :placeholder="placeholder" :required="required ? true : false"></textarea>
+    <textarea v-model="model" :id="id" :rows="rows ? rows : '4'"
+        :class="resize === 'none' ? 'resize-none' : ''" class="col-span-2 block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+        :placeholder="placeholder" :required="required ? true : false" :disabled="disabled ? true : false"></textarea>
 </template>
